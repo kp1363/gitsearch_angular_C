caff488904b84bcd0371a7e018074fd795b6cdf1
kp1363 <keyurp1363@gmail.com>
2017-10-25 13:43:51 -0700

    1st
    1st commit

---
+++ src/app/component/github.component.ts
@@ -0,0 +1,35 @@
+﻿import { Component } from '@angular/core';
+import {GithubService} from '../services/github.service';
+
+@Component({
+    moduleId:module.id,
+    selector: 'github',
+    templateUrl: `github.component.html`,
+    providers: [GithubService]
+})
+export class GithubComponent {
+
+    user: any;
+    repos: any;
+    username: string;
+
+    constructor(private _githubService: GithubService) {
+        console.log('init');
+        
+           
+    }
+
+    search() {
+        this._githubService.updateUsername(this.username);
+
+        this._githubService.getUser().subscribe(user => {
+            //console.log(user)
+            this.user = user;
+        });
+
+        this._githubService.getRepos().subscribe(repos => {
+            //console.log(user)
+            this.repos = repos;
+        });
+    }
+}
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,31 @@
+﻿import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+@Injectable()
+export class GithubService {
+    private username = 'keyur1363';
+    private client_id = '70cf2ced0b41b93c821f';
+    private client_secret = '04ede1bea6d8fdc69d10b5a4feaa47feaa5666a4';
+
+    constructor(private _http: Http) {
+        console.log('service init');
+
+    }
+
+    getUser() {
+       return this._http.get('https://api.github.com/users/' + this.username)
+            .map(response => response.json());
+    }
+    getRepos() {
+        return this._http.get('https://api.github.com/users/' + this.username+'/repos')
+            .map(response => response.json());
+    }
+
+    updateUsername(username: string) {
+        this.username = username;
+    }
+
+}
